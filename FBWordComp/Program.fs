18a20b73303e6d18c6bc81288515231ac8e655ed
Haukur Óskar Þorgeirsson <haukuroskar@meniga.com>
2017-11-10 11:07:15 +0000

    First commit, argument reading works

---
+++ FBWordComp/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    System.Console.ReadLine () |> ignore
+    0 // return an integer exit code
