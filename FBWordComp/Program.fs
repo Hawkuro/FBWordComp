9defba1a4a31a88d5d5f9f65dda9be590fc36191
Haukur Óskar Þorgeirsson <haukuroskar@meniga.com>
2017-11-10 11:28:12 +0000

    Reading of message folder implemented

--- FBWordComp/Program.fs
+++ FBWordComp/Program.fs
@@ -1,8 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System
+open System.Configuration
+open System.IO
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+
+    let FBDataFolder = ConfigurationManager.AppSettings.Item("FBDataFolder") |> DirectoryInfo
+    let messageFolder = FBDataFolder.GetDirectories() |> Array.find (fun d -> d.Name = "messages")
+    let messageThreads = messageFolder.GetFiles()
+
+    messageThreads
+    |> Array.map (fun f -> f.Name)
+    |> printfn "%A" 
+
     System.Console.ReadLine () |> ignore
     0 // return an integer exit code
